96e2f84faac8df46e9bba4d5c6b0fddd272e299a
Ruslan <arnov@i.ua>
2025-06-20 15:41:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  base: '/goiteens-hw-js-vite/', //! 👈 ВАЖЛИВО: вкажіть базу
+  base: '/hw-js-vite/', //! 👈 ВАЖЛИВО: вкажіть базу
   root: 'src',
   build: {
     rollupOptions: {
